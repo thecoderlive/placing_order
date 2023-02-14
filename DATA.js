e02ff2167560eaea99e4832eb3d171f1f3c8e1ab
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-14 13:41:58 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,47 @@
+export const item = {
+    "id": "d39f5bf8-541f-442c-a741-833bec2667fa",
+    "item_name": "true",
+    "popular_with": "stuff",
+    "watch_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/359.png",
+    "min_count": 7415,
+    "tag_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/226.png",
+    "about_offer": "Authority himself yet wear west. Wonder enter break number father drop set between. Country nothing color.",
+    "veg_only": "my",
+    "food_list": [
+        {
+            "id": "5ebaa1b8-48cf-4564-859f-f226b7c13ee5",
+            "food_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/292.png",
+            "food_name": "Become trade great mean. Environmental plan myself avoid brother one.",
+            "food_ingredients": "Language where thank stay capital. Discover expert ahead hot particularly fill.",
+            "food_price": 5992
+        },
+        {
+            "id": "fd70a60a-6fab-4e12-af19-86812c1f0654",
+            "food_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/84.png",
+            "food_name": "Political from study officer certainly. Threat coach around station.",
+            "food_ingredients": "Think hospital look interesting fact most. Together half ago state modern.",
+            "food_price": 7989
+        },
+        {
+            "id": "71dd4da1-3303-46ac-8e69-4a560cd2d04c",
+            "food_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/142.png",
+            "food_name": "Down property crime whose.",
+            "food_ingredients": "Side bank dinner begin eye hospital. Condition practice information throughout end politics activity. Western age somebody business contain free staff identify.",
+            "food_price": 291
+        },
+        {
+            "id": "7c2f1a9e-5797-43d7-b11d-d4f43a2fe7b1",
+            "food_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/27.png",
+            "food_name": "Individual maybe main quite scene. Run career successful try campaign above town. Address real a fear true trial side when.",
+            "food_ingredients": "Husband discover account what. Many her child.",
+            "food_price": 8271
+        },
+        {
+            "id": "d79fa774-895c-4bd8-a8af-1511690fbd83",
+            "food_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/109.png",
+            "food_name": "Between face behind election two next. But claim together information foot another approach.",
+            "food_ingredients": "Service mouth instead next possible. Family site man bit decision.",
+            "food_price": 3136
+        }
+    ]
+}
